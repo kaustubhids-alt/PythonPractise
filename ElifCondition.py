ca8eab6d293a2d55a55d33b4598cc0fbb221cc58
kaustubhids-alt <kaustubhi.ds@gmail.com>
2026-05-11 18:13:55 +0530

    practise for if else, elif condition

---
+++ ElifCondition.py
@@ -0,0 +1,8 @@
+number= 0
+
+if number>0 :
+ print("Number is Positive")
+elif number<0 :
+ print("Number is Negative")
+else :
+ print("Number is 0")
---
+++ Vote.py
@@ -0,0 +1,7 @@
+age = 4
+
+if age>=18 :
+ print("Yes, you can vote")
+
+else :
+ print("No, You can't vote")
