a13bcfc9fe993d1f8929ce535c64862aee791a14
kaustubhids-alt <kaustubhi.ds@gmail.com>
2026-05-12 19:54:36 +0530

    checking vowels using Elif and in operator

---
+++ ElifVowels.py
@@ -0,0 +1,18 @@
+letter = "b"
+if letter=="a":
+    print("It is Vowel")
+
+elif letter =="e":
+    print("It is Vowel")
+
+elif letter =="i":
+    print("It is Vowel")    
+
+elif letter =="o":
+    print("It is Vowel")
+
+elif letter =="u":
+    print("It is Vowel")
+
+else :
+    print("It is Consonant")
---
+++ ElifVowelwithInOperator.py
@@ -0,0 +1,6 @@
+letter = "b"
+if letter in "aeiouAEIOU":
+    print("It is Vowel")
+
+else :
+    print("It is Consonant")
