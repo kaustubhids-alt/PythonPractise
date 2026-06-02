c47528b23b9e85c60be5d12fd0ab6f22dd0b7a5c
kaustubhids-alt <kaustubhi.ds@gmail.com>
2026-06-03 00:18:36 +0530

    function practise

---
+++ function_divisibility.py
@@ -0,0 +1,8 @@
+def divisibility():
+    a = int(input("Enter A Number: "))
+    if a % 3 == 0:
+        print("Divisible by 3")
+    else:
+        print("Not divisible by 3")
+
+divisibility()
---
+++ function_odd_even.py
@@ -0,0 +1,8 @@
+def odd_even_checker():
+    a = int(input("Enter A Number: "))
+    if a % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+odd_even_checker()
