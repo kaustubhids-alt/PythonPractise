7aff7f1264e5e23fa96030f5c1238c38865d1a27
kaustubhids-alt <kaustubhi.ds@gmail.com>
2026-06-24 15:58:12 +0530

    class practise

---
+++ class_rectangle_area.py
@@ -0,0 +1,17 @@
+class Rectangle:
+
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+a = Rectangle(15, 25)
+print(a.perimeter)
+print(a.area)
+        
+
